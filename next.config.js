5d845590de2125191198bbdc89c0ad66fb09c489
Olle Solens <raproid@gmail.com>
2022-09-05 16:47:42 +0300

    set up assetPrefix

--- next.config.js
+++ next.config.js
@@ -11,7 +11,6 @@ module.exports = withNextra({
     loader: 'akamai',
     path: '',
   },
-  basePath: '/docs',
   trailingSlash: true,
   assetPrefix: isProd ? 'https://raproid-github-io.vercel.app/' : undefined,
 });
